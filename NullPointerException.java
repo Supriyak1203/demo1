4597aaf32e890e0a94765968d760c368f907635a
Supriyak1203 <supriyakale643@gmail.com>
2025-10-02 14:26:21 +0530

    Exception

---
+++ NullPointerException.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class NullPointerException {
+    public static void main(String[] args) {
+        
+    
+    String str=null;
+
+    System.out .println(str.length());
+    
+    }
+    
+    
+}
