fc188060bb6c7d6f6fb754276a5fabb276c699f2
Supriyak1203 <supriyakale643@gmail.com>
2025-10-04 22:42:23 +0530

    Synchronized

--- Day 2/MultiThreading/Counter.java
+++ Day 2/MultiThreading/Counter.java
@@ -1,11 +1,40 @@
 public class  Counter {
      private int count=0;
-    public synchronized  void increment() {
+    public   void increment() {
+        synchronized(this){
          count++;
+        }
     }
 
     public int getCount(){
         return count;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter c=new Counter();
+        MyThread t1=new MyThread(c);
+        MyThread t2=new MyThread(c);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(c.getCount());
+       
+
+    }
+}
+
+class MyThread extends Thread{
+    private Counter counter;
+
+    MyThread(Counter counter){
+           this.counter=counter;
+    }
+    @Override
+    public void run(){
+        for(int i=0;i<=10; i++)
+        counter.increment();
     }
 }
 
